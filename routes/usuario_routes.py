9d9d1d4caa693e0b3deb0011c03e27ecb8ac8961
Ricardo Maroquio <maroquio@gmail.com>
2025-10-15 19:17:23 -0300

    consertado erro na rota /usuarios

--- routes/usuario_routes.py
+++ routes/usuario_routes.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter, Request
 from util.template_util import criar_templates
 from util.auth_decorator import requer_autenticacao
@@ -8,11 +9,12 @@ templates_usuario = criar_templates("templates")
 
 @router.get("/usuario")
 @requer_autenticacao()
-async def dashboard(request: Request, usuario_logado: dict):
+async def dashboard(request: Request, usuario_logado: Optional[dict] = None):
     """
     Dashboard do usuário (área privada)
     Requer autenticação
     """
+    assert usuario_logado is not None
     return templates_usuario.TemplateResponse(
         "home.html",
         {
